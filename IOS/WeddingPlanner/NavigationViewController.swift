7b90c7f5dccaedcbffe25b29736b28c0ede7aa5b
felipeuntill <felipeassuncao.net@gmail.com>
2016-04-21 06:02:05 -0300

    some update

---
+++ IOS/WeddingPlanner/NavigationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SidebarTableViewController.swift
+//  WeddingPlanner
+//
+//  Created by Felipe Assunção on 4/20/16.
+//  Copyright © 2016 Felipe Assunção. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UIViewController {
+
+ 
+
+}
