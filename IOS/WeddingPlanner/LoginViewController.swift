0de6e9c4caa6ae6a91b849b6db7062a2d81e1f82
felipeuntill <felipeassuncao.net@gmail.com>
2016-04-20 22:38:02 -0300

    coding

---
+++ IOS/WeddingPlanner/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  WeddingPlanner
+//
+//  Created by Felipe Assunção on 4/19/16.
+//  Copyright © 2016 Felipe Assunção. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+//    @IBAction func openmenu(sender: AnyObject) {
+//        revealViewController().revealToggle(sender);
+//    }
+    
+}
