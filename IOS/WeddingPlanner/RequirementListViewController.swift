1863432825e254d1c0c309dc27841b753acd0a1a
felipeuntill <felipeassuncao.net@gmail.com>
2016-04-21 18:08:02 -0300

    working in another views

---
+++ IOS/WeddingPlanner/RequirementListViewController.swift
@@ -0,0 +1,67 @@
+//
+//  RequirementViewController.swift
+//  WeddingPlanner
+//
+//  Created by Felipe Assunção on 4/21/16.
+//  Copyright © 2016 Felipe Assunção. All rights reserved.
+//
+
+import Foundation
+
+class RequirementListViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if revealViewController() != nil {
+            self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("requirement")! as! RequirementTableViewCell
+        
+        cell.titleLabel.text = "\(indexPath.row)"
+        
+        if(indexPath.item % 2 == 0)
+        {
+            cell.backgroundColor = UIColor.clearColor()
+        }
+        else
+        {
+            cell.backgroundColor = UIColor.whiteColor()
+                .colorWithAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.whiteColor()
+                .colorWithAlphaComponent(0.0)
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if(editingStyle == .Delete)
+        {
+            //RecipeManager.DeleteRecipe(indexPath.item)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if(segue.identifier == "detailSegue"){
+//            let cell = sender as! CustomTableViewCell
+//            let detailview = segue.destinationViewController as! DetailViewController
+//            let id = RecipeManager.recipes.indexOf({ $0.title == cell.recipe?.title })
+//            let recipe = RecipeManager.LoadRecipe(id!)
+//            detailview.recipe = recipe
+//        }
+//    }
+    
+}
---
+++ IOS/WeddingPlanner/RequirementTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RequirementViewCell.swift
+//  WeddingPlanner
+//
+//  Created by Felipe Assunção on 4/21/16.
+//  Copyright © 2016 Felipe Assunção. All rights reserved.
+//
+
+import UIKit
+
+class RequirementTableViewCell : UITableViewCell {
+    
+    
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    
+    var requirement : Requirement?
+    
+    
+}
